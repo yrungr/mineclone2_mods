db35e8bf8023286fb333890ef2de5aa0101d6b45
magnus michaelson <magnusmichaelson@protonmail.com>
2020-05-16 14:53:30 +0100

    :)

---
+++ simple_block/init.lua
@@ -0,0 +1,12 @@
+print("hej")
+minetest.register_node("yrungr:squares", {
+    description = "yrungr brick",
+    tiles = {
+        "minetest_block.png",
+        "minetest_block.png",
+        "minetest_block.png",
+        "minetest_block.png",
+        "minetest_block.png",
+        "minetest_block.png"
+    },
+})
